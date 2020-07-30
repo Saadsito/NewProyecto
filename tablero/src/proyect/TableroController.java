a3e3f573d34f007ea57904ea81d84918b01320ce
Jose Saad <josemiguelsaad13@gmail.com>
2020-07-30 16:58:00 -0400

    cambiostablero

--- tablero/src/proyect/TableroController.java
+++ tablero/src/proyect/TableroController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -200,6 +201,92 @@ public class TableroController implements Initializable {
     private ImageView espada22111111;
     @FXML
     private ImageView espada221112;
+    @FXML
+    private ImageView fichainv;
+    @FXML
+    private ImageView cascoinv;
+    @FXML
+    private ImageView petoinv;
+    @FXML
+    private ImageView espadainv;
+    @FXML
+    private ImageView pantaloninv;
+    @FXML
+    private ImageView botasinv;
+    @FXML
+    private ImageView inv1;
+    @FXML
+    private ImageView inv2;
+    @FXML
+    private ImageView inv3;
+    @FXML
+    private ImageView inv4;
+    @FXML
+    private ImageView inv5;
+    @FXML
+    private ImageView inv6;
+    @FXML
+    private ImageView inv7;
+    @FXML
+    private ImageView inv8;
+    @FXML
+    private ImageView dado;
+    @FXML
+    private ImageView bestia;
+    @FXML
+    private ImageView direccion;
+    @FXML
+    private ImageView ficha1;
+    @FXML
+    private ImageView ficha2;
+    @FXML
+    private ImageView ficha3;
+    @FXML
+    private ImageView ficha4;
+    @FXML
+    private ImageView ficha5;
+    @FXML
+    private ImageView ficha6;
+    @FXML
+    private ImageView posib1;
+    @FXML
+    private ImageView posib3;
+    @FXML
+    private ImageView posib2;
+    @FXML
+    private ImageView posib4;
+    @FXML
+    private ImageView posib5;
+    @FXML
+    private ImageView lapiznegro;
+    @FXML
+    private ImageView lapizamarillo;
+    @FXML
+    private ImageView equiscasco;
+    @FXML
+    private ImageView equispeto;
+    @FXML
+    private ImageView equisespada;
+    @FXML
+    private ImageView equispantalon;
+    @FXML
+    private ImageView equisbotas;
+    @FXML
+    private ImageView equisinv1;
+    @FXML
+    private ImageView equisinv2;
+    @FXML
+    private ImageView equisinv3;
+    @FXML
+    private ImageView equisinv4;
+    @FXML
+    private ImageView equisinv5;
+    @FXML
+    private ImageView equisinv6;
+    @FXML
+    private ImageView equisinv7;
+    @FXML
+    private ImageView equisinv8;
 
     /**
      * Initializes the controller class.
@@ -208,5 +295,168 @@ public class TableroController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clickposib1(MouseEvent event) {
+    }
+
+    @FXML
+    private void clickposib3(MouseEvent event) {
+    }
+
+    @FXML
+    private void clickposib2(MouseEvent event) {
+    }
+
+    @FXML
+    private void clickposib4(MouseEvent event) {
+    }
+
+    @FXML
+    private void clickposib5(MouseEvent event) {
+    }
+
+    @FXML
+    private void clicklapiznegro(MouseEvent event) {
+        lapiznegro.setVisible(false);
+        lapizamarillo.setVisible(true);
+        equiscasco.setVisible(true);
+        equispeto.setVisible(true);
+        equisespada.setVisible(true);
+        equispantalon.setVisible(true);
+        equisbotas.setVisible(true);
+        equisinv1.setVisible(true);
+        equisinv2.setVisible(true);
+        equisinv3.setVisible(true);
+        equisinv4.setVisible(true);
+        equisinv5.setVisible(true);
+        equisinv6.setVisible(true);
+        equisinv7.setVisible(true);
+        equisinv8.setVisible(true);
+    }
+
+    @FXML
+    private void clicklapizamarillo(MouseEvent event) {
+        lapiznegro.setVisible(true);
+        lapizamarillo.setVisible(false);
+        equiscasco.setVisible(false);
+        equispeto.setVisible(false);
+        equisespada.setVisible(false);
+        equispantalon.setVisible(false);
+        equisbotas.setVisible(false);
+        equisinv1.setVisible(false);
+        equisinv2.setVisible(false);
+        equisinv3.setVisible(false);
+        equisinv4.setVisible(false);
+        equisinv5.setVisible(false);
+        equisinv6.setVisible(false);
+        equisinv7.setVisible(false);
+        equisinv8.setVisible(false);
+    }
+
+    @FXML
+    private void equiscascoclick(MouseEvent event) {
+    }
+
+    @FXML
+    private void equispetoclick(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisespadaclick(MouseEvent event) {
+    }
+
+    @FXML
+    private void equispantalonclick(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisbotasclick(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv1click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv2click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv3click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv4click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv5click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv6click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv7click(MouseEvent event) {
+    }
+
+    @FXML
+    private void equisinv8click(MouseEvent event) {
+    }
+
+    @FXML
+    private void dadoclick(MouseEvent event) {
+        int numdado = (int) (Math.random() * 6) + 1;
+    }
+
+    @FXML
+    private void ficha1abajo(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha1arriba(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha2abajo(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha2arriba(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha3abajo(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha3arriba(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha4abajo(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha4arriba(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha5abajo(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha5arriba(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha6abajo(MouseEvent event) {
+    }
+
+    @FXML
+    private void ficha6arriba(MouseEvent event) {
+    }
     
 }
